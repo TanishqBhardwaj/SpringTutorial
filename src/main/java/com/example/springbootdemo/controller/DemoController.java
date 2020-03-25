29b74f1d11d5c6d35f7b0fdc883c81b36c75fddc
MrTan1999 <tanishqbhardwaj.tb@gmail.com>
2020-03-25 22:59:08 +0530

    Add controller

---
+++ src/main/java/com/example/springbootdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DemoController {
+    private Map<String, Object> result = new HashMap<>();
+
+    @RequestMapping("/demo1")
+    public Map<String, Object> hello() {
+        result.put("name", "Tanishq");
+        result.put("city", "Kanpur");
+        return result;
+    }
+}
